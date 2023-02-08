a3a5b6e61b419be99bdfac26e162ae2654b30702
Sara McKim <mckimsa@sheridancollege.ca>
2023-02-08 17:05:20 -0500

    second commit

---
+++ src/exercise1/Card.java
@@ -0,0 +1,34 @@
+package exercise1;
+
+/**
+ * A class that models playing card Objects. Cards have 
+ * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
+ * A suit (clubs, hearts, spades, diamonds).
+ * There are 52 cards in a deck, no jokers.
+ * This code is to be used in ICE1. When you create your own branch,
+ * add your name as a modifier.
+ * @author dancye
+ * @author Paul Bonenfant May 2020
+ */
+
+public class Card {
+
+     int value;
+     String suit;
+    
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+}
